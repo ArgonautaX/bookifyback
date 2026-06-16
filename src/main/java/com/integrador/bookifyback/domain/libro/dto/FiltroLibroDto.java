dc9e9bf1601363754838e0de49299fe6b67f56d4
ErwinSmth <jesusd9perez@gmail.com>
2026-06-16 01:17:36 -0500

    Correcciones

--- src/main/java/com/integrador/bookifyback/domain/libro/dto/FiltroLibroDto.java
+++ src/main/java/com/integrador/bookifyback/domain/libro/dto/FiltroLibroDto.java
@@ -3,12 +3,12 @@ package com.integrador.bookifyback.domain.libro.dto;
 import java.math.BigDecimal;
 
 public record FiltroLibroDto(
-        String titulo,
-        Long autorId,
-        String autorNombre,
-        Long categoriaId,
-        String categoriaNombre,
-        BigDecimal precioMin,
-        BigDecimal precioMax,
-        String formato
-) {}
+                String titulo,
+                Long autorId,
+                String autorNombre,
+                Long categoriaId,
+                String categoriaNombre,
+                BigDecimal precioMin,
+                BigDecimal precioMax,
+                String formato) {
+}
